405b8c31232103063e54973495d1fad51512923b
zienna <simone.zienna@gmail.com>
2018-02-10 18:24:12 -0800

    Swift features implemented

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -8,13 +8,53 @@
 
 import Foundation
 
-class Concentration {
+struct Concentration {
     
     var cards = Array<Card>()
     
-    //picks card to return
-    func chooseCard(at index: Int) {
+    var indexOfTheOneAndOnlyCardFaceUp: Int? {
+        get {
+            //filter returns an Array that applied a function (or closure) to each element 
+            return cards.indices.filter {cards[$0].isFaceUp}.oneAndOnly
+//            let faceUpCardIndices = cards.indices.filter {cards[$0].isFaceUp}
+//            return faceUpCardIndices.count == 1 ? faceUpCardIndices.first : nil
+            
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    //one card is set up so update index
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        //two cards are up now so restore to nil
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
+        }
         
+        set(newValue) {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+    
+    //picks card to return
+    mutating func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index is not in the cards")
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfTheOneAndOnlyCardFaceUp, matchIndex != index {
+                if cards[matchIndex] == cards[index] {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            } else {
+                indexOfTheOneAndOnlyCardFaceUp = index
+            }
+        }
     }
     
     init(numberOfPairsOfCards: Int) {
@@ -30,4 +70,10 @@ class Concentration {
     }
     
     
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
 }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
 
     //gets initialized only when used but cannot
     //set didSet{} on lazy vars
-    lazy var game = Concentration(numberOfPairsOfCards:
-        (cardButtons.count + 1) / 2)
+    lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    //not settable already without keyword private(set)
+    var numberOfPairsOfCards: Int {
+        get {
+            return (cardButtons.count + 1) / 2
+        }
+    }
+    
     
     var flipCount : Int = 0 {
         didSet {
@@ -21,28 +28,50 @@ class ViewController: UIViewController {
         }
     }
    
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
             game.chooseCard(at: cardNumber)
             updateViewFromModel()
-        } else {
-            print("chosen card was not in cardButtons")
         }
-    
-        flipCard(withEmoji: "👻", on: sender)
-        
     }
     
-    func updateViewFromModel() {
-        
+    private func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
+        }
     }
     
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
+    //private var emojiChoices = ["👻", "🎃", "🦇", "🙀", "😈", "😱", "🍭", "🍬"]
+    private var emojiChoices = "👻🎃🦇🙀😈😱🍭🍬";
+
+    
+    private var emoji = [Card:String]()
+    
+    private func emoji(for card:Card) -> String {
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            //this code doesn't work cuz Strings are not indexed by int but by String.Index
+            //let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: Int(arc4random_uniform(UInt32(emojiChoices.count))))
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
+        }
+        //returns optional if it's set otherwise returns "?"
+        return emoji[card] ?? "?"
+    }
+    
+    private func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
@@ -51,6 +80,13 @@ class ViewController: UIViewController {
             button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         }
         
+    }
+    
+    override func viewDidLoad() {
+        for index in cardButtons.indices {
+            cardButtons[index].setTitle("", for: UIControlState.normal)
+            cardButtons[index].backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
     }
     
 }
